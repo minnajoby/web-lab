715c974e449785dbdb01035721b6445074f90fa6
minnajoby <24mp2277@rit.ac.in>
2024-11-15 13:55:34 +0530

    Add files via upload

---
+++ backgroundcolor.js
@@ -0,0 +1,8 @@
+const button=document.getElementById("colorButton");
+button.addEventListener('click',function(){
+	document.body.style.backgroundColor="red";
+});
+button.addEventListener('dblclick',function(){
+	document.body.style.backgroundColor="green";
+});
+
